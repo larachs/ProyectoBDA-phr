cb7089cb5260a2f389648d97599b9c8763ea10a9
larachs <raespinoza@estudiantec.cr>
2024-09-22 15:42:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 SummitSphere, gestionamos tu evento!")
+
+
+
+# Título de la página
+st.write('Cargar múltiples archivos')
+
+# Descripción
+st.write('Por favor, sube tus archivos, para gestionar de la mejor manera tu evento')
+
+# Botón para subir archivos
+archivos_subidos = st.file_uploader("Seleccionar archivos", accept_multiple_files=True)
+
+# Si se suben archivos, mostrar detalles
+if archivos_subidos:
+    st.write(f"Se han subido {len(archivos_subidos)} archivos.")
+    for archivo in archivos_subidos:
+        st.write(f"Nombre: {archivo.name}, Tipo: {archivo.type}, Tamaño: {archivo.size} bytes")
