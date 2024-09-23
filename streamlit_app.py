5c9876854ace7dcd7faba980c721de1a13c551b7
larachs <raespinoza@estudiantec.cr>
2024-09-22 22:34:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,38 +3,14 @@ import streamlit as st
 st.title("🎈SummitSphere, gestionamos tu evento!")
 
 
-import os
+
+
 from neo4j import GraphDatabase
-import dotenv
 
+# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+URI = "bolt://localhost:7687"
+AUTH = ("neo4j", "PabloHilaRache")
 
-
-# Cargar las variables de entorno desde un archivo .env
-dotenv.load_dotenv("Neo4j-a0a2fa1d-Created-2023-11-06.txt")
-
-# Obtener la URI y las credenciales de Neo4j desde las variables de entorno
-URI = os.getenv("bolt://localhost:7687")
-USERNAME = os.getenv("neo4j")
-PASSWORD = os.getenv("PabloHilaRache")
-
-# Función para conectar y verificar la conexión con Neo4j
-def conectar_neo4j(uri, user, password):
-    try:
-        driver = GraphDatabase.driver(uri, auth=(user, password))
-        driver.verify_connectivity()  # Verifica la conectividad
-        st.success("Conexión a Neo4j establecida.")
-        return driver
-    except Exception as e:
-        st.error(f"Error al conectar con Neo4j: {e}")
-        return None
-
-# Conectar a Neo4j
-driver = conectar_neo4j(URI, USERNAME, PASSWORD)
-
-# Cerrar la conexión cuando ya no sea necesaria
-if driver:
-    with driver.session() as session:
-        st.write("Ejecutando consultas en Neo4j...")
-        # Aquí puedes agregar tus consultas a la base de datos usando el session
-        # Ejemplo: session.run("MATCH (n) RETURN n")
-    driver.close()  # Cerrar el driver
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    driver.verify_connectivity()
+    print("Connection established.")
