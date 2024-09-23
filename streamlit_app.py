dfaaa35b6929c181d39f6a9b471aca6dbaedc448
larachs <145821267+larachs@users.noreply.github.com>
2024-09-22 23:43:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ from neo4j import GraphDatabase
 # Configuración de la conexión a Neo4j
 uri = "bolt://localhost:7687"  # Utiliza el puerto Bolt que te proporcionaron
 driver = GraphDatabase.driver(uri, auth=("neo4j", "PabloHilaRache"))  # Reemplaza "password" con tu contraseña
-print("Conexion en progreso")
+
 
