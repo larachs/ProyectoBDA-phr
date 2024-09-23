897daeb59963f6af4361426de1ee14c047bc79de
larachs <raespinoza@estudiantec.cr>
2024-09-23 14:12:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,15 @@ from neo4j.exceptions import ServiceUnavailable
 st.title("🎈SummitSphere, gestionamos tu evento!")
 
 
-# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
-URI = "bolt://localhost[:7474]/browser/"
-AUTH = ("neo4j", "PabloHilaRache")
 
-with GraphDatabase.driver(URI, auth=AUTH) as driver:
-    driver.verify_connectivity()
-    print("Connection established.")
-    st.write("Connection established")
+URI = "bolt://localhost:7687"
+AUTH = ("neo4j", "PabloHilaRache")  # Tus credenciales
+
+try:
+    with GraphDatabase.driver(URI, auth=AUTH) as driver:
+        driver.verify_connectivity()
+        print("Conexión exitosa a Neo4j.")
+        st.write("Conexión exitosa a Neo4j.")
+except Exception as e:
+    print(f"Error al conectarse a Neo4j: {e}")
+    st.write(f"Error al conectarse a Neo4j: {e}")
