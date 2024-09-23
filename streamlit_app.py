f91292c6c05da509fc1c622d8ee3d1158aacb6e9
larachs <raespinoza@estudiantec.cr>
2024-09-22 22:58:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,7 @@ import streamlit as st
 
 st.title("🎈SummitSphere, gestionamos tu evento!")
 
-########SUBIR ARCHIVOS 
-    # Descripción
-st.write('Por favor, sube tus archivos.')
 
-    # Botón para subir archivos
-archivos_subidos = st.file_uploader("Selecciona archivos", accept_multiple_files=True)
-
-    # Si se suben archivos, mostrar detalles
-if archivos_subidos:
-    st.write(f"Se han subido {len(archivos_subidos)} archivos.")
-    for archivo in archivos_subidos:
-        st.write(f"Nombre: {archivo.name}, Tipo: {archivo.type}, Tamaño: {archivo.size} bytes")
 
 ##### SUPUESTA CONEXION
 from neo4j import GraphDatabase
