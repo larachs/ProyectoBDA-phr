fc4f2a2c03e2a006e2b424bc9815b84f9046d43b
larachs <raespinoza@estudiantec.cr>
2024-09-23 13:59:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,48 +6,16 @@ st.title("🎈SummitSphere, gestionamos tu evento!")
 
 
 # Configuración de la conexión a Neo4jj
-uri = "bolt+s://localhost:7687"  # Utiliza el puerto Bolt que te proporcionaron
+uri = "bolt://localhost:7687"  # Utiliza el puerto Bolt que te proporcionaron
 driver = GraphDatabase.driver(uri, auth=("neo4j", "PabloHilaRache"))  # Reemplaza "password" con tu contraseña
 
 
 
-def guardar_archivos_en_neo4j(archivos_subidos):
-    try:
-        with driver.session() as session:
-            for archivo in archivos_subidos:
-                session.write_transaction(
-                    crear_nodo_archivo,
-                    archivo.name,
-                    archivo.type,
-                    archivo.size
-                )
-                st.write(f"Archivo '{archivo.name}' guardado en la base de datos.")
-    except ServiceUnavailable as e:
-        st.error("Error al conectarse a Neo4j: " + str(e))
-    except Exception as e:
-        st.error("Ocurrió un error: " + str(e))
+# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+URI = "<bolt://localhost:7687>"
+AUTH = ("<neo4j>", "<PabloHilaRache>")
 
-
-def crear_nodo_archivo(tx, nombre, tipo, tamaño):
-    # Crear un nodo con las propiedades del archivo
-    tx.run(
-        """
-        CREATE (a:Archivo {nombre: $nombre, tipo: $tipo, tamaño: $tamaño})
-        """,
-        nombre=nombre, tipo=tipo, tamaño=tamaño
-    )
-
-# Descripción
-st.write('Por favor, sube tus archivos para guardarlos en Neo4j')
-
-# Botón para subir archivos
-archivos_subidos = st.file_uploader("Selecciona archivos", accept_multiple_files=True)
-
-# Botón para ejecutar la carga a Neo4j
-if st.button("Guardar en Neo4j"):
-    if archivos_subidos:
-        guardar_archivos_en_neo4j(archivos_subidos)
-    else:
-        st.warning("No se han subido archivos aún.")
-
-driver.close()
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    driver.verify_connectivity()
+    print("Connection established.")
+    st.write("Connection established")
