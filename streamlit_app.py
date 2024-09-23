e642acec731dae4a3987341994ca81bbbc640a7d
larachs <raespinoza@estudiantec.cr>
2024-09-22 22:45:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,29 +4,34 @@ st.title("🎈SummitSphere, gestionamos tu evento!")
 
 
 
-# Descripción
-st.write('Por favor, sube tus archivos.')
+def conexionBDA(): 
+    URI="bolt://localhost:7687"
+    NEO4J_USER="neo4j"
+    NEO4J_PASSWORD="PabloHilaRache"
 
-# Botón para subir archivos
-archivos_subidos = st.file_uploader("Selecciona archivos", accept_multiple_files=True)
+    # Descripción
+    st.write('Por favor, sube tus archivos.')
 
-# Si se suben archivos, mostrar detalles
-if archivos_subidos:
-    st.write(f"Se han subido {len(archivos_subidos)} archivos.")
-    for archivo in archivos_subidos:
-        st.write(f"Nombre: {archivo.name}, Tipo: {archivo.type}, Tamaño: {archivo.size} bytes")
+    # Botón para subir archivos
+    archivos_subidos = st.file_uploader("Selecciona archivos", accept_multiple_files=True)
 
-import dotenv
-import os
-from neo4j import GraphDatabase
+    # Si se suben archivos, mostrar detalles
+    if archivos_subidos:
+        st.write(f"Se han subido {len(archivos_subidos)} archivos.")
+        for archivo in archivos_subidos:
+            st.write(f"Nombre: {archivo.name}, Tipo: {archivo.type}, Tamaño: {archivo.size} bytes")
 
-load_status = dotenv.load_dotenv("Neo4j-a0a2fa1d-Created-2023-11-06.txt")
-if load_status is False:
-    raise RuntimeError('Environment variables not loaded.')
+    import dotenv
+    import os
+    from neo4j import GraphDatabase
 
-URI = os.getenv("bolt://localhost:7687")
-AUTH = (os.getenv("neo4j"), os.getenv("PabloHilaRache"))
+    load_status = dotenv.load_dotenv("Neo4j-a0a2fa1d-Created-2023-11-06.txt")
+    if load_status is False:
+        raise RuntimeError('Environment variables not loaded.')
 
-with GraphDatabase.driver(URI, auth=AUTH) as driver:
-    driver.verify_connectivity()
-    print("Connection established.")
+    URI = os.getenv("bolt://localhost:7687")
+    AUTH = (os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD"))
+
+    with GraphDatabase.driver(URI, auth=AUTH) as driver:
+        driver.verify_connectivity()
+        print("Connection established.")
